d25397f2fd14ae08f12637cc1568bf6d24f556f9
= <vovaherdman@gmail.com>
2025-07-03 13:17:08 -0400

    King and knight possible moves

--- src/game/logic/bit_boards.cpp
+++ src/game/logic/bit_boards.cpp
@@ -4,44 +4,36 @@
 #include "game/logic/bit_boards.hpp"
 
 namespace {
-    //usings
+    //Usings
     using U64 = std::uint64_t;
 
-    //variables
+    //Variables
     constexpr U64 FILE_A = 0x8080808080808080ULL;
+    constexpr U64 FILE_AB = (FILE_A | 0x4040404040404040ULL);
     constexpr U64 FILE_H = 0x0101010101010101ULL;
+    constexpr U64 FILE_GH = (0x0202020202020202ULL | FILE_H);
 
     constexpr U64 RANK_1 = 0x00000000000000FFULL;
     constexpr U64 RANK_8 = 0xFF00000000000000ULL;
 
     //Functions
     constexpr U64 leftShift(const U64 b) {return b << 1;}
+    U64 getSidePieces(BITBOARDS::Side side) {
+        return side.pawns | side.bishops | side.knights | side.king | side.queens | side.rooks;
+    }
     
-    constexpr U64 kingMoves(U64 king) {
-        return (king << 1) & (~FILE_H) |
-            (king >> 1) & (~FILE_A)    |
-            (king << 8)                |
-            (king >> 8)                |
-            (king << 7) & (~FILE_A)    |
-            (king >> 7) & (~FILE_H)    |
-            (king << 9) & (~FILE_H)    |
-            (king >> 9) & (~FILE_A);
+    constexpr U64 kingMoves(U64 piece) {
+        return  ((piece << 1) & (~FILE_H)    | (piece >> 1) & (~FILE_A)  |
+                 (piece << 8)                | (piece >> 8)              |
+                 (piece << 7) & (~FILE_A)    | (piece >> 7) & (~FILE_H)  |
+                 (piece << 9) & (~FILE_H)    | (piece >> 9) & (~FILE_A)) & (~getSidePieces(BITBOARDS::white));
     }
-
-    #ifdef DEV_HELPERS_LATER_REMOVE
-    void printBitBoard(U64 num) {
-        for (int i = 0; i < 64; i++) {
-            // Check first digit of num and print out, move on to next
-            if ((num & 0x8000000000000000) == 0x8000000000000000) {
-                std::cout << "1 ";
-            } else {std::cout << "0 ";}
-            num = leftShift(num);
-            if ((i+1) % 8 == 0) {
-                std::cout << '\n';
-            }
-        }
+    constexpr U64 knightMoves(U64 piece) {
+        return  ((((piece >> 6)  | (piece << 10)) & ~FILE_GH) |
+                 (((piece >> 10) | (piece << 6))  & ~FILE_AB) |
+                 (((piece >> 15) | (piece << 17)) & ~FILE_H)  |
+                 (((piece >> 17) | (piece << 15)) & ~FILE_A)) & (~getSidePieces(BITBOARDS::white));
     }
-    #endif
 }
 
 namespace BITBOARDS {
@@ -59,6 +51,19 @@ namespace BITBOARDS {
 }
 
 #ifdef DEV_HELPERS_LATER_REMOVE
+void printBitBoard(U64 num) {
+    for (int i = 0; i < 64; i++) {
+        // Check first digit of num and print out, move on to next
+        if ((num & 0x8000000000000000) == 0x8000000000000000) {
+            std::cout << "1 ";
+        } else {std::cout << "0 ";}
+        num = leftShift(num);
+        if ((i+1) % 8 == 0) {
+            std::cout << '\n';
+        }
+    }
+}
+
 void bitboardDevFunc() {
     U64 whiteSide = 
         BITBOARDS::white.pawns |
@@ -68,7 +73,9 @@ void bitboardDevFunc() {
         BITBOARDS::white.queens |
         BITBOARDS::white.king;
     //printBitBoard(whiteSide);
-    constexpr U64 king = 0x0000000008000000ULL;
-    printBitBoard(kingMoves(king));
+    //constexpr U64 king = 0x0000000008000000ULL;
+    //printBitBoard(kingMoves(king));
+    U64 piece = 0x0000000002000000ULL;
+    printBitBoard(knightMoves(piece));
 }
 #endif
